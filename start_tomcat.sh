4ab43db407ec25097aedb633dfa403301544bd93
iamvickyav <vickyavw.10@gmail.com>
2021-05-09 23:04:03 +0530

    start tomcat script added

---
+++ start_tomcat.sh
@@ -0,0 +1,13 @@
+echo "*** TOMCAT SCRIPT EXECUTION START ***"
+echo "*** INPUT PATH: $1 ***"
+DIR=$1
+if [ -d "$DIR" ]; then
+  cd $1
+  cd bin
+  echo "*** ENTERING TOMCAT bin DIRECTORY: $(pwd) ***"
+  echo "*** STARTING TOMCAT WITH catalina.sh start COMMAND ***"
+  ./catalina.sh start
+else
+  echo "*** INVALID PATH INPUT ***"
+fi
+  echo "*** TOMCAT SCRIPT EXECUTION END ***"
